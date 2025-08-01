c787d46f6a74869b5b7b18e0ce4601880a8f2269
Mr.Darshan Dakhane <168203751+darshan0421@users.noreply.github.com>
2025-08-01 10:53:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const actionBtn = document.getElementById('actionBtn');
+
+
+actionBtn.addEventListener('click', () => {
+  
+  alert('Button clicked! Enjoy your adventure!');
+
+
+  document.querySelector('.title').textContent = 'Adventure Awaits!';
+});
